71c9e5bac36e478d36e945adb062d0bfa43cf2e9
LORENZO LEAL MARANHO <lorenzo.lmaranho@senacsp.edu.br>
2024-02-28 21:41:41 -0300

    Primeiro Commit

---
+++ calculadoraTI20N/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadoraTI20N
+{
+    class model
+    {
+        // area de variaveis
+        private double num1;
+        private double num2; 
+
+
+
+
+            //metodo construror = instancia  as variaveis 
+
+
+            public Model()
+        {
+
+            num1 = 0 
+            num2 = 0
+
+
+        }//fim do construtor
+
+
+        //metodos GET E SET 
+        public double ConsultarNum1
+        {
+            get { return num1; }
+            set { num1 = value; }
+        }//fim do get e set 
+        
+        
+        
+        
+        
+        }
+
+
+    public double Tabuada()
+    {
+       string tabuada()
+        {
+            string resultado = "";
+
+            for(int i=0; i <= 10; i++)
+        }
+        resultado += ConsultarNum1 + " * " + i + " = " + (ConsultarNum1 * i);  
+
+        
+        
+        
+        for (int i = 0; i <= 10; i += 3)
+        {
+            ConsultarNum1* i; 
+
+
+            
+
+
+
+
+
+
+
+        }//fim do for
+   }//fim do metodo tabuada
+
+    public string Raiz()
+    {
+       resultado = "A raiz de" + ConsultarNum1 + " é: " +  Math.Sqrt(ConsultarNum1); + 
+                    "\nA raiz de " + ConsultarNum2 + "  é"
+   
+
+
+
+
+
+   }//fim da raiz 
+
+
+
+
+
+
+
+}//fim da classe
+}//fim do projeto
+                                                  
